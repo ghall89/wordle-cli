a77b8d2a429988d1d9020e9b3661e2dcfa1b40f8
Graham Hall <ghall89@me.com>
2022-10-30 19:37:30 -0400

    show correct guess in grid

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,11 +1,14 @@
 import chalk from 'chalk';
 import inquirer from 'inquirer';
 
-// import dictionary
+// import stuff
 import words from './dictionary.js';
+import drawGrid from './drawGrid';
 
 // set word from game dictionary
-const word = words[Math.floor(Math.random() * words.length)];
+// const word = words[Math.floor(Math.random() * words.length)];
+
+const word = 'apple';
 
 // this defines the game grid
 const guesses = [
@@ -22,32 +25,7 @@ let won = false;
 
 // primary game loop
 const gameLoop = index => {
-	// draw game grid
-	console.log(`┌───┬───┬───┬───┬───┐`);
-	console.log(
-		`│ ${guesses[0][0]} │ ${guesses[0][1]} │ ${guesses[0][2]} │ ${guesses[0][3]} │ ${guesses[0][4]} │`,
-	);
-	console.log(`├───┼───┼───┼───┼───┤`);
-	console.log(
-		`│ ${guesses[1][0]} │ ${guesses[1][1]} │ ${guesses[1][2]} │ ${guesses[1][3]} │ ${guesses[1][4]} │`,
-	);
-	console.log(`├───┼───┼───┼───┼───┤`);
-	console.log(
-		`│ ${guesses[2][0]} │ ${guesses[2][1]} │ ${guesses[2][2]} │ ${guesses[2][3]} │ ${guesses[2][4]} │`,
-	);
-	console.log(`├───┼───┼───┼───┼───┤`);
-	console.log(
-		`│ ${guesses[3][0]} │ ${guesses[3][1]} │ ${guesses[3][2]} │ ${guesses[3][3]} │ ${guesses[3][4]} │`,
-	);
-	console.log(`├───┼───┼───┼───┼───┤`);
-	console.log(
-		`│ ${guesses[4][0]} │ ${guesses[4][1]} │ ${guesses[4][2]} │ ${guesses[4][3]} │ ${guesses[4][4]} │`,
-	);
-	console.log(`├───┼───┼───┼───┼───┤`);
-	console.log(
-		`│ ${guesses[5][0]} │ ${guesses[5][1]} │ ${guesses[5][2]} │ ${guesses[5][3]} │ ${guesses[5][4]} │`,
-	);
-	console.log(`└───┴───┴───┴───┴───┘`);
+	drawGrid(guesses);
 
 	// lose state
 	if (index === 6) {
@@ -116,6 +94,8 @@ const gameLoop = index => {
 
 			// win state
 			if (guess.toLowerCase() === word) {
+				console.clear();
+				drawGrid(guesses);
 				if (index === 0) {
 					console.log('Wow! You won on the first guess!');
 				} else {
