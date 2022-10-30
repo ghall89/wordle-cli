2c54f050a19e700867b04a52228d6fd25f6d83df
Graham Hall <ghall89@me.com>
2022-10-30 16:16:55 -0400

    adjustments to game display

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -17,8 +17,34 @@ const guesses = [
 let won = false;
 
 const gameLoop = index => {
-	if (index === 5) {
-		console.log('Out of guesses!');
+	console.log(`┌───┬───┬───┬───┬───┐`);
+	console.log(
+		`│ ${guesses[0][0]} │ ${guesses[0][1]} │ ${guesses[0][2]} │ ${guesses[0][3]} │ ${guesses[0][4]} │`,
+	);
+	console.log(`├───┼───┼───┼───┼───┤`);
+	console.log(
+		`│ ${guesses[1][0]} │ ${guesses[1][1]} │ ${guesses[1][2]} │ ${guesses[1][3]} │ ${guesses[1][4]} │`,
+	);
+	console.log(`├───┼───┼───┼───┼───┤`);
+	console.log(
+		`│ ${guesses[2][0]} │ ${guesses[2][1]} │ ${guesses[2][2]} │ ${guesses[2][3]} │ ${guesses[2][4]} │`,
+	);
+	console.log(`├───┼───┼───┼───┼───┤`);
+	console.log(
+		`│ ${guesses[3][0]} │ ${guesses[3][1]} │ ${guesses[3][2]} │ ${guesses[3][3]} │ ${guesses[3][4]} │`,
+	);
+	console.log(`├───┼───┼───┼───┼───┤`);
+	console.log(
+		`│ ${guesses[4][0]} │ ${guesses[4][1]} │ ${guesses[4][2]} │ ${guesses[4][3]} │ ${guesses[4][4]} │`,
+	);
+	console.log(`├───┼───┼───┼───┼───┤`);
+	console.log(
+		`│ ${guesses[5][0]} │ ${guesses[5][1]} │ ${guesses[5][2]} │ ${guesses[5][3]} │ ${guesses[5][4]} │`,
+	);
+	console.log(`└───┴───┴───┴───┴───┘`);
+
+	if (index === 6) {
+		console.log(`Out of guesses! The word was ${chalk.underline(word)}.`);
 		return;
 	}
 
@@ -71,32 +97,6 @@ const gameLoop = index => {
 				}
 			}
 
-			console.log(`┌───┬───┬───┬───┬───┐`);
-			console.log(
-				`│ ${guesses[0][0]} │ ${guesses[0][1]} │ ${guesses[0][2]} │ ${guesses[0][3]} │ ${guesses[0][4]} │`,
-			);
-			console.log(`├───┼───┼───┼───┼───┤`);
-			console.log(
-				`│ ${guesses[1][0]} │ ${guesses[1][1]} │ ${guesses[1][2]} │ ${guesses[1][3]} │ ${guesses[1][4]} │`,
-			);
-			console.log(`├───┼───┼───┼───┼───┤`);
-			console.log(
-				`│ ${guesses[2][0]} │ ${guesses[2][1]} │ ${guesses[2][2]} │ ${guesses[2][3]} │ ${guesses[2][4]} │`,
-			);
-			console.log(`├───┼───┼───┼───┼───┤`);
-			console.log(
-				`│ ${guesses[3][0]} │ ${guesses[3][1]} │ ${guesses[3][2]} │ ${guesses[3][3]} │ ${guesses[3][4]} │`,
-			);
-			console.log(`├───┼───┼───┼───┼───┤`);
-			console.log(
-				`│ ${guesses[4][0]} │ ${guesses[4][1]} │ ${guesses[4][2]} │ ${guesses[4][3]} │ ${guesses[4][4]} │`,
-			);
-			console.log(`├───┼───┼───┼───┼───┤`);
-			console.log(
-				`│ ${guesses[5][0]} │ ${guesses[5][1]} │ ${guesses[5][2]} │ ${guesses[5][3]} │ ${guesses[5][4]} │`,
-			);
-			console.log(`└───┴───┴───┴───┴───┘`);
-
 			if (guess.toLowerCase() === word) {
 				if (index === 0) {
 					console.log('Wow! You won on the first guess!');
@@ -108,6 +108,7 @@ const gameLoop = index => {
 		})
 		.finally(() => {
 			if (won === false) {
+				console.clear();
 				gameLoop(index + 1);
 			}
 		});
